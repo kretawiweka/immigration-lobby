7a24afc61bc1180c07a0c4e5447be5be44a51492
Kretawiweka Sani <kretawiweka.sani@shopee.com>
2020-11-26 04:26:00 +0700

    add laporan page and basic information page.

---
+++ src/pages/manage-service-information/index.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { Select, Table, Space, Button } from 'antd';
+
+import Layout from '../../components/Layout';
+import { SingleHeader } from '../../components/Header';
+import { SelectOptionArea, Content, TableArea, AddBtnArea } from './style';
+const { Option } = Select;
+const { Column } = Table;
+
+const ManageServiceInformation = () => {
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  const data = [
+    {
+      key: '1',
+      indo_service: 'Layanan Indonesia',
+      eng_service: 'Layanan Inggris',
+      indo_description: 32,
+      eng_description: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      indo_service: 'Layanan Indonesia',
+      eng_service: 'Layanan Inggris',
+      indo_description: 32,
+      eng_description: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      indo_service: 'Layanan Indonesia',
+      eng_service: 'Layanan Inggris',
+      indo_description: 32,
+      eng_description: 'New York No. 1 Lake Park',
+    },
+  ];
+  return (
+    <>
+      <Layout>
+        <SingleHeader color="#fafafa" title="KELOLA INFORMASI LAYANAN" />
+        <Content>
+          <SelectOptionArea>
+            <h4>Kategori Informasi</h4>
+            <Select
+              defaultValue="Pilih Kategori Informasi"
+              style={{ width: '200px' }}
+              onChange={handleChange}
+            >
+              <Option value="Jenis Layanan Keimigrasian">
+                Jenis Layanan Keimigrasian
+              </Option>
+              <Option value="Jenis Produk">Jenis Produk</Option>
+              <Option value="Jenis Permohonan">Jenis Permohonan</Option>
+              <Option value="Biaya Permohonan">Biaya Permohonan</Option>
+              <Option value="Tujuan permohonan">Tujuan Permohonan</Option>
+              <Option value="Dokumen Persyaratan">Dokumen Persyaratan</Option>
+              <Option value="Tahapan Permohonan">Tahapan Permohonan</Option>
+            </Select>
+          </SelectOptionArea>
+          <SelectOptionArea>
+            <h4>Jenis Layanan</h4>
+            <Select
+              defaultValue="Pilih Jenis Layanan"
+              style={{ width: '200px' }}
+              onChange={handleChange}
+            >
+              <Option value="Paspor">Paspor</Option>
+              <Option value="Izin Tinggal">Izin Tinggal</Option>
+              <Option value="Visa">Visa</Option>
+            </Select>
+          </SelectOptionArea>
+          <TableArea>
+            <AddBtnArea>
+              <Button>Tambah</Button>
+            </AddBtnArea>
+            <Table dataSource={data}>
+              <Column
+                title="Nama Layanan Indo"
+                dataIndex="indo_service"
+                key="indo_service"
+              />
+              <Column
+                title="Nama Layanan Inggris"
+                dataIndex="eng_service"
+                key="eng_service"
+              />
+              <Column
+                title="Deskripsi Indonesia"
+                dataIndex="indo_description"
+                key="indo_description"
+              />
+              <Column
+                title="Deskripsi Inggris"
+                dataIndex="eng_description"
+                key="eng_description"
+              />
+              <Column
+                title="Action"
+                key="action"
+                render={(text, record) => (
+                  <Space size="middle">
+                    <a>Edit</a>
+                    <a>Delete</a>
+                  </Space>
+                )}
+              />
+            </Table>
+          </TableArea>
+        </Content>
+      </Layout>
+    </>
+  );
+};
+
+export default ManageServiceInformation;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import Home from './pages/home';
 import Login from './pages/login';
+/** /informasi layanan */
 import ServiceInformation from './pages/service-information';
+/** /status-permohonan */
 import ApplicationStatus from './pages/application-status';
 import ApplicationStatusPassport from './pages/application-status/passport';
+/** /laporan */
+import Report from './pages/report';
+/** /kelola-informasi-layanan */
+import ManageServiceInformation from './pages/manage-service-information';
 
 const routes = [
   {
@@ -18,12 +24,20 @@ const routes = [
     page: ServiceInformation,
   },
   {
+    path: '/kelola-informasi-layanan',
+    page: ManageServiceInformation,
+  },
+  {
     path: '/status-permohonan',
     page: ApplicationStatus,
   },
   {
     path: '/status-permohonan/paspor',
     page: ApplicationStatusPassport,
+  },
+  {
+    path: '/laporan',
+    page: Report,
   },
 ];
 
