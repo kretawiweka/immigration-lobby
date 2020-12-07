2974c27985e68e11641594567a874c8b81ed8471
Kretawiweka Sani <kretawiweka.sani@shopee.com>
2020-12-07 16:08:29 +0700

    Improve manage-service-information logic

--- src/pages/manage-service-information/index.jsx
+++ src/pages/manage-service-information/index.jsx
@@ -1,147 +1,85 @@
-import React, { useState } from 'react';
-import { Select, Table, Space, Button } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Select } from 'antd';
 
 import Layout from '../../components/Layout';
 import { SingleHeader } from '../../components/Header';
-import { ModalCreate, ModalUpdate, ModalDelete } from './ModalData';
-import { SelectOptionArea, Content, TableArea, AddBtnArea } from './style';
+import { SelectOptionArea, Content } from './style';
+import Service from './service';
+import ProductInformation from './product-information';
+import Product from './product';
+
 const { Option } = Select;
-const { Column } = Table;
 
 const ManageServiceInformation = () => {
-  const [isVisibleModalCreate, setIsVisibleModalCreate] = useState(false);
-  const [isVisibleModaUpdate, setIsVisibleModalUpdate] = useState(false);
-  const [isVisibleModalDelete, setIsVisibleModalDelete] = useState(false);
+  const [conditionState, setConditionState] = useState('service');
+  const [serviceSelect, setServiceSelect] = useState('');
+  const [productInformationSelect, setProductInformationSelect] = useState('');
 
-  const dataTable = [
-    {
-      key: '1',
-      indo_service: 'Layanan Indonesia',
-      eng_service: 'Layanan Inggris',
-      indo_description: 32,
-      eng_description: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      indo_service: 'Layanan Indonesia',
-      eng_service: 'Layanan Inggris',
-      indo_description: 32,
-      eng_description: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      indo_service: 'Layanan Indonesia',
-      eng_service: 'Layanan Inggris',
-      indo_description: 32,
-      eng_description: 'New York No. 1 Lake Park',
-    },
-  ];
+  useEffect(() => {
+    if (serviceSelect === '') {
+      setConditionState('service');
+    } else if (productInformationSelect === '') {
+      setConditionState('product');
+    } else {
+      setConditionState('product_information');
+    }
+  }, [serviceSelect, productInformationSelect]);
 
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
+  const serviceHandleChange = (value) => {
+    setServiceSelect(value);
   };
 
-  const onChangeModalCreate = () => {
-    setIsVisibleModalCreate(!isVisibleModalCreate);
+  const productHandleChange = (value) => {
+    setProductInformationSelect(value);
   };
 
-  const onChangelModalUpdate = () => {
-    setIsVisibleModalUpdate(!isVisibleModaUpdate);
-  };
-
-  const onChangeModalDelete = () => {
-    setIsVisibleModalDelete(!isVisibleModalDelete);
+  const DataTable = () => {
+    switch (conditionState) {
+      case 'product':
+        return <Product />;
+      case 'product_information':
+        return <ProductInformation />;
+      default:
+        return <Service />;
+    }
   };
 
   return (
     <>
       <Layout>
-        <ModalCreate
-          isVisible={isVisibleModalCreate}
-          onCancel={onChangeModalCreate}
-        />
-        <ModalUpdate
-          isVisible={isVisibleModaUpdate}
-          onCancel={onChangelModalUpdate}
-        />
-        <ModalDelete
-          isVisible={isVisibleModalDelete}
-          onCancel={onChangeModalDelete}
-        />
         <SingleHeader title="KELOLA INFORMASI LAYANAN" />
         <Content>
-          <SelectOptionArea>
-            <h4>Kategori Informasi</h4>
-            <Select
-              defaultValue="Pilih Kategori Informasi"
-              style={{ width: '200px' }}
-              onChange={handleChange}
-            >
-              <Option value="Jenis Layanan Keimigrasian">
-                Jenis Layanan Keimigrasian
-              </Option>
-              <Option value="Jenis Produk">Jenis Produk</Option>
-              <Option value="Jenis Permohonan">Jenis Permohonan</Option>
-              <Option value="Biaya Permohonan">Biaya Permohonan</Option>
-              <Option value="Tujuan permohonan">Tujuan Permohonan</Option>
-              <Option value="Dokumen Persyaratan">Dokumen Persyaratan</Option>
-              <Option value="Tahapan Permohonan">Tahapan Permohonan</Option>
-            </Select>
-          </SelectOptionArea>
           <SelectOptionArea>
             <h4>Jenis Layanan</h4>
             <Select
               defaultValue="Pilih Jenis Layanan"
               style={{ width: '200px' }}
-              onChange={handleChange}
+              onChange={serviceHandleChange}
             >
+              <Option value="">Semua</Option>
               <Option value="Paspor">Paspor</Option>
-              <Option value="Izin Tinggal">Izin Tinggal</Option>
-              <Option value="Visa">Visa</Option>
             </Select>
           </SelectOptionArea>
-          <TableArea>
-            <AddBtnArea>
-              <Button onClick={onChangeModalCreate}>Tambah</Button>
-            </AddBtnArea>
-            <Table dataSource={dataTable}>
-              <Column
-                title="Nama Layanan Indonesia"
-                dataIndex="indo_service"
-                key="indo_service"
-              />
-              <Column
-                title="Nama Layanan Inggris"
-                dataIndex="eng_service"
-                key="eng_service"
-              />
-              <Column
-                title="Deskripsi Indonesia"
-                dataIndex="indo_description"
-                key="indo_description"
-              />
-              <Column
-                title="Deskripsi Inggris"
-                dataIndex="eng_description"
-                key="eng_description"
-              />
-              <Column
-                title="Action"
-                key="action"
-                render={(text, record) => (
-                  <Space size="middle">
-                    <Button onClick={onChangelModalUpdate} type="link">
-                      Ubah
-                    </Button>
-                    <Button onClick={onChangeModalDelete} type="link">
-                      Hapus
-                    </Button>
-                  </Space>
-                )}
-              />
-            </Table>
-          </TableArea>
+          {conditionState !== 'service' && (
+            <SelectOptionArea>
+              <h4>Jenis Produk</h4>
+              <Select
+                defaultValue="Pilih Jenis Produk"
+                style={{ width: '200px' }}
+                onChange={productHandleChange}
+              >
+                <Option value="">Semua</Option>
+                <Option value="Paspor Biasa 24 Halaman">
+                  Paspor Biasa 24 Halaman
+                </Option>
+                <Option value="Paspor Biasa 48 Halaman">
+                  Paspor Biasa 48 Halaman
+                </Option>
+              </Select>
+            </SelectOptionArea>
+          )}
         </Content>
+        <DataTable />
       </Layout>
     </>
   );
--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,29 +1,6 @@
-const getTtlType = (time, ttlType) => {
-  switch (ttlType) {
-    case 'minute':
-      return time * 60;
-    case 'hour':
-      return time * 3600;
-    case 'day':
-      return time * 86400;
-    case 'week':
-      return time * 86400 * 7;
-    default:
-      return time;
-  }
-};
-
-const setLocalStorage = (key, value, ttl = { time: 0, type: null }) => {
-  const now = new Date();
-  const expiry =
-    ttl.time === 0 || ttl.type === null
-      ? null
-      : now.getTime() + getTtlType(ttl.time, ttl.type);
+const setLocalStorage = (key, value) => {
   const item = {
-    value: {
-      ...value,
-    },
-    expiry,
+    value,
   };
   localStorage.setItem(key, JSON.stringify(item));
 };
@@ -34,11 +11,7 @@ const getLocalStorage = (key) => {
     return null;
   }
   const item = JSON.parse(itemStr);
-  const now = new Date();
-  if (now.getTime() > item.expiry) {
-    localStorage.removeItem(key);
-    return null;
-  }
+
   return item.value;
 };
 
