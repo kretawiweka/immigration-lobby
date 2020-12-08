19fb4d613ec6d88e2e71643924134f2ba62a6438
Kretawiweka Sani <kretawiweka.sani@shopee.com>
2020-12-08 19:19:14 +0700

    deaktivasi akun modal

--- src/pages/user-account/index.jsx
+++ src/pages/user-account/index.jsx
@@ -1,102 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Layout from '../../components/Layout';
 import { ContentUserAccount, BreadCrumbContainer } from './style';
 
-import { Breadcrumb, Typography, Button, Row, Col, Table, Space } from 'antd';
+import {
+  Breadcrumb,
+  Modal,
+  Typography,
+  Button,
+  Row,
+  Col,
+  Table,
+  Space,
+} from 'antd';
 
 import { DeleteOutlined } from '@ant-design/icons';
 const { Title } = Typography;
-
-const columns = [
-  {
-    title: 'No.',
-    dataIndex: 'no',
-  },
-  {
-    title: 'NIP',
-    dataIndex: 'nip',
-    sorter: {
-      compare: (a, b) => a.nik - b.nik,
-      multiple: 8,
-    },
-  },
-  {
-    title: 'Nama',
-    dataIndex: 'nama',
-    sorter: {
-      compare: (a, b) => a.nama - b.nama,
-      multiple: 8,
-    },
-  },
-  {
-    title: 'Username',
-    dataIndex: 'username',
-    sorter: {
-      compare: (a, b) => a.username - b.username,
-      multiple: 8,
-    },
-  },
-  {
-    title: 'Hak Akses',
-    dataIndex: 'hakakses',
-    sorter: {
-      compare: (a, b) => a.hakakses - b.hakakses,
-      multiple: 8,
-    },
-  },
-  {
-    title: 'Kantor Imigrasi',
-    dataIndex: 'kanim',
-    sorter: {
-      compare: (a, b) => a.kanim - b.kanim,
-      multiple: 8,
-    },
-  },
-  {
-    title: 'Jabatan',
-    dataIndex: 'jabatan',
-    sorter: {
-      compare: (a, b) => a.jabatan - b.jabatan,
-      multiple: 8,
-    },
-  },
-  {
-    title: 'Pangkat/Gol.',
-    dataIndex: 'pangkatgol',
-    sorter: {
-      compare: (a, b) => a.pangkatgol - b.pangkatgol,
-      multiple: 8,
-    },
-  },
-  {
-    title: 'Terakhir Login',
-    dataIndex: 'lastlogin',
-    sorter: {
-      compare: (a, b) => a.lastlogin - b.lastlogin,
-      multiple: 8,
-    },
-  },
-  {
-    title: 'Aksi',
-    dataIndex: 'aksi',
-    key: 'aksi',
-    render: (text, record) => (
-      <Space size="middle">
-        <Button
-          htmlType="submit"
-          className="login-form-button"
-          icon={<DeleteOutlined />}
-          size="small"
-          danger
-        >
-          Deaktivasi
-        </Button>
-      </Space>
-    ),
-  },
-];
 
 const data = [
   {
@@ -160,9 +82,159 @@ function onChange(pagination, filters, sorter, extra) {
   console.log('params', pagination, filters, sorter, extra);
 }
 
+const DeaktivasiModal = ({ isVisible, onCancel }) => {
+  return (
+    <Modal
+      title="Deaktivasi Akun Pengguna"
+      visible={isVisible}
+      onCancel={onCancel}
+      footer={[]}
+    >
+      <ToastContainer />
+      <h4>Apakah Anda yakin akan mendeaktviasi akun pengguna ini?</h4>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          marginTop: '35px',
+        }}
+      >
+        <Button
+          onClick={() => {
+            toast.error('Akun berhasil dideaktivasi', {
+              position: 'top-right',
+              autoClose: 3000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            });
+            setTimeout(function () {
+              onCancel();
+            }, 3000);
+          }}
+          style={{ width: '100px' }}
+          type="primary"
+        >
+          Ya
+        </Button>
+        <Button onClick={onCancel} style={{ width: '100px' }}>
+          Tidak
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
 const UserAccount = () => {
+  const [isVisibleDeaktivasiModal, setIsVisibleModalDelete] = useState(false);
+  const [deaktivasiData, setDeaktivasiData] = useState({});
+
+  const onChangeDeaktivasiModal = (record) => {
+    setDeaktivasiData(record);
+    setIsVisibleModalDelete(!isVisibleDeaktivasiModal);
+  };
+
+  const columns = [
+    {
+      title: 'No.',
+      dataIndex: 'no',
+    },
+    {
+      title: 'NIP',
+      dataIndex: 'nip',
+      sorter: {
+        compare: (a, b) => a.nik - b.nik,
+        multiple: 8,
+      },
+    },
+    {
+      title: 'Nama',
+      dataIndex: 'nama',
+      sorter: {
+        compare: (a, b) => a.nama - b.nama,
+        multiple: 8,
+      },
+    },
+    {
+      title: 'Username',
+      dataIndex: 'username',
+      sorter: {
+        compare: (a, b) => a.username - b.username,
+        multiple: 8,
+      },
+    },
+    {
+      title: 'Hak Akses',
+      dataIndex: 'hakakses',
+      sorter: {
+        compare: (a, b) => a.hakakses - b.hakakses,
+        multiple: 8,
+      },
+    },
+    {
+      title: 'Kantor Imigrasi',
+      dataIndex: 'kanim',
+      sorter: {
+        compare: (a, b) => a.kanim - b.kanim,
+        multiple: 8,
+      },
+    },
+    {
+      title: 'Jabatan',
+      dataIndex: 'jabatan',
+      sorter: {
+        compare: (a, b) => a.jabatan - b.jabatan,
+        multiple: 8,
+      },
+    },
+    {
+      title: 'Pangkat/Gol.',
+      dataIndex: 'pangkatgol',
+      sorter: {
+        compare: (a, b) => a.pangkatgol - b.pangkatgol,
+        multiple: 8,
+      },
+    },
+    {
+      title: 'Terakhir Login',
+      dataIndex: 'lastlogin',
+      sorter: {
+        compare: (a, b) => a.lastlogin - b.lastlogin,
+        multiple: 8,
+      },
+    },
+    {
+      title: 'Aksi',
+      dataIndex: 'aksi',
+      key: 'aksi',
+      render: (text, record) => (
+        <Space size="middle">
+          <Button
+            htmlType="submit"
+            className="login-form-button"
+            icon={<DeleteOutlined />}
+            size="small"
+            danger
+            onClick={() => {
+              onChangeDeaktivasiModal(record);
+            }}
+          >
+            Deaktivasi
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
   return (
     <>
+      <DeaktivasiModal
+        isVisible={isVisibleDeaktivasiModal}
+        onCancel={onChangeDeaktivasiModal}
+        data={deaktivasiData}
+      />
       <Layout>
         <ContentUserAccount>
           <BreadCrumbContainer>
